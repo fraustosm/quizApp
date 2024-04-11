108ccca31e84d03b9e14f3b6de5b3eb79b535654
María Frausto <mfrausto.fs@gmail.com>
2024-04-11 00:16:05 -0700

    start

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("startButton").addEventListener("click", function() {
+    // Show the quiz container
+    document.getElementById("quizContainer").style.display = "block";
+    // Hide the start button
+    this.style.display = "none";
+});
